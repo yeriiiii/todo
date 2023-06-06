ef7a7e359cbe75d33c04c9c924a1e32c450feea7
yeriiiii <yerina98@gmail.com>
2023-06-07 02:02:44 +0900

    fix: task 완료 기능 서버와 연동

--- todoling/src/components/main/list/DoneTasks.vue
+++ todoling/src/components/main/list/DoneTasks.vue
@@ -1,15 +1,11 @@
 <template>
   <div>
-    <div class="flex items-center p-2 mt-2 text-xl font-bold text-blue-400">
-      DONE
-    </div>
+    <div class="flex items-center p-2 mt-2 text-xl font-bold text-blue-400">DONE</div>
     <ul class="my-1 space-y-3 text-gray-400 text-md">
       <li v-for="task in taskStore.getDoneTasks" :key="task.id">
-        <div
-          class="flex items-center w-full p-3 bg-gray-100 h-13 justify-content"
-        >
+        <div class="flex items-center w-full p-3 bg-gray-100 h-13 justify-content">
           <div
-            @click="toggleThisTask(task.id)"
+            @click="toggleThisTask(task.id, task.text, task.dueDate)"
             class="w-5 h-5 ml-1 mr-4 bg-gray-200 border-2 hover:bg-gray-50"
           ></div>
           <div>{{ task.text }}</div>
@@ -32,7 +28,6 @@ import { defineComponent } from "vue";
 import { useTaskStore } from "../../../stores/taskStore.js";
 import { deleteTask, putTask } from "../../../api/http-module";
 
-
 export default defineComponent({
   props: {
     sortOption: String,
@@ -40,14 +35,19 @@ export default defineComponent({
   setup() {
     const taskStore = useTaskStore();
 
-    const toggleThisTask = (id: string) => {
-      taskStore.toggleTask(id);
+    const toggleThisTask = async (id: string, text: string, dueDate: string) => {
+      const response = await putTask(id, text, dueDate, false);
+      if (response.status === 200) {
+        taskStore.toggleTask(id);
+      } else {
+        alert("put request failure");
+        console.error(response);
+      }
     };
 
     const updateThisTask = async (id: string, text: string, dueDate: string, isDone: boolean) => {
       const response = await putTask(id, text, dueDate, isDone);
       if (response.status === 200) {
-        console.log(response);
       } else {
         alert("put request failure");
         console.error(response);
--- todoling/src/components/main/list/TodoTasks.vue
+++ todoling/src/components/main/list/TodoTasks.vue
@@ -1,15 +1,11 @@
 <template>
   <div>
-    <div class="flex items-center p-2 mt-2 text-xl font-bold text-red-400">
-      TODO
-    </div>
+    <div class="flex items-center p-2 mt-2 text-xl font-bold text-red-400">TODO</div>
     <ul class="my-1 space-y-3 text-md">
       <li v-for="task in taskStore.getTodoTasks" :key="task.id">
-        <div
-          class="flex items-center justify-between w-full p-3 border-2 border-indigo-100 h-13"
-        >
+        <div class="flex items-center justify-between w-full p-3 border-2 border-indigo-100 h-13">
           <div
-            @click="toggleThisTask(task.id)"
+            @click="toggleThisTask(task.id, task.text, task.dueDate)"
             class="w-5 h-5 ml-1 mr-4 border-2 border-indigo-200 hover:bg-indigo-100"
           ></div>
           <div class="text-slate-700">{{ task.text }}</div>
@@ -31,14 +27,20 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useTaskStore } from "../../../stores/taskStore.js";
-import { deleteTask } from "../../../api/http-module";
+import { deleteTask, putTask } from "../../../api/http-module";
 
 export default defineComponent({
   setup() {
     const taskStore = useTaskStore();
 
-    const toggleThisTask = (id: string) => {
-      taskStore.toggleTask(id);
+    const toggleThisTask = async (id: string, text: string, dueDate: string) => {
+      const response = await putTask(id, text, dueDate, true);
+      if (response.status === 200) {
+        taskStore.toggleTask(id);
+      } else {
+        alert("put request failure");
+        console.error(response);
+      }
     };
 
     const removeThisTask = async (id: string) => {
